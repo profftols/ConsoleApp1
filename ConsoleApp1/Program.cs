14e5e987383fead6b7880f43647ed4519d95b53c
Ваня Кориков <profftols@yandex.ru>
2022-03-12 18:37:06 +0500

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[6];
+
+            Console.WriteLine(array[^1]); 
+        }
+    }
+}
