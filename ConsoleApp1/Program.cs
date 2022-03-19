15e6c5738ec9ef1c7be03d4a06f44893daff48d9
Ваня Кориков <profftols@yandex.ru>
2022-03-19 20:28:44 +0500

    pain

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -9,10 +9,20 @@ namespace ConsoleApp1
 {
     class Program
     {
+        static void Uni(ref int a)
+        {
+            a = 5;
+        }
         static void Main(string[] args)
         {
+            string s = "пустота";
             int[] sim = {1, 4, 6, 7, 8, 9 };
             Console.WriteLine(sim[0]);
+
+            int num = 99;
+            Uni(ref num);
+
+            Console.WriteLine(num);
         }
     }
 }
