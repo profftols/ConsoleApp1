e59932062d3d4784635aa5973895ab4eb2986625
Ваня Кориков <profftols@yandex.ru>
2022-03-22 21:04:48 +0500

    такс я устал

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -15,7 +15,6 @@ namespace ConsoleApp1
             Console.Write($"{a[i]} ");
 
             k += a[i];
-
             i++;
 
             if (i == a.Length)
@@ -27,13 +26,25 @@ namespace ConsoleApp1
 
             Uni(a, i, k);
         }
+
+        static int Sum (int a)
+        {
+            if (a < 10)
+                return a;
+            int digit = a % 10;
+            int nextA = a / 10;
+
+            return digit + Sum(nextA);
+        }
         static void Main(string[] args)
         {
+            int result = 561;
             //int i = 0;
             //int k = 0;
-            int[] sim = { 1, 4, 6, 7, 8, 9 };
+            int[] sim = { 1, 4, 6 };
             Console.Write("вывод массива: ");
             Uni(sim);
+            Console.WriteLine(Sum(result));
         }
     }
 }
