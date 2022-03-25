84c2ee929a2de636f10a5a40537d5cd50e13e4d8
[GeNrI16] <[Denisow16@yandex.ru]>
2022-03-25 20:27:04 +0500

    DZ 3 я сделяль

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -9,33 +9,29 @@ namespace ConsoleApp1
 {
     class Program
     {
-        static void Sumint(int i)
+        static int Sumint(int i)
         {
             int c;
             int d;
             int e;
-            int f;
-            i *= 100;
-            //Console.WriteLine(c);
-            Console.WriteLine(i);
-            i *= 10;
-            Console.WriteLine(i);
-            /*d = c %= 10;
-            Console.WriteLine(d);
-            e = d % 1;
-            Console.WriteLine(e);
-            f = c + d + e;
-            Console.WriteLine(f);*/
+            if (i <= 9)
+            {
+                return i;
 
+            }
+            c = i % 10;
+            d = i / 10;
+            e = c + Sumint(d);
 
-
-
+            return e;
+            
         }
         static void Main(string[] args)
         {
             int i = 123;
-            Sumint(i);
-
+            int f;
+            f = Sumint(i);
+            Console.WriteLine(f);
         }
 
 
