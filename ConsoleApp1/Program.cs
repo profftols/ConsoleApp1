9f116b9724a2525c1f0766a4c0e1ac2b17f5402b
[GeNrI16] <[Denisow16@yandex.ru]>
2022-03-31 20:35:10 +0500

    temperatura

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -9,33 +9,60 @@ namespace ConsoleApp1
 {
     class Program
     {
-        static int Sumint(int i)
-        {
-            int c;
-            int d;
-            int e;
-            if (i <= 9)
-            {
-                return i;
 
-            }
-            c = i % 10;
-            //Console.WriteLine(c);
-            d = i / 10;
-            //Console.WriteLine(d);
-            e = c + Sumint(d);
-            //Console.WriteLine(e);
-            return e;
-            
+        enum Season
+        {
+            Winter,
+            Vesna,
+            Summer,
+            Autumn
         }
         static void Main(string[] args)
         {
-            int i = 123;
-            int f;
-            f = Sumint(i);
-            Console.WriteLine(f);
-        }
+            Random rand = new Random();
 
+            int[] temp = new int[9];
+            for (int i = 0; i < temp.Length; i++)
+            {
+                temp[i] = rand.Next(-25, 40);
+            }
+
+            //int[] temp = { -5, 0, 2, 6 };
+            Forchseason(temp);
+            
+        }
+        static void Forchseason(int[] temp)
+        {
+            int colt = 0;
+            int polo = 0;
+
+            for (int i = 0; i < temp.Length; i++)
+            {
+                if (temp[i] < 0)
+                {
+                    colt++;
+                }
+                if (temp[i] > 0)
+                {
+                    polo++;
+                }                           
+            }
+
+            if (colt > polo)
+            {
+                Console.WriteLine((Season)0);
+            }
+            if (polo > colt)
+            {
+                Console.WriteLine((Season)2);
+            }
+            if (colt == polo)
+            {
+                Console.WriteLine((Season)1);
+                Console.WriteLine((Season)3);
+            }
+            
+        }
 
     }
 }
--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -4,24 +4,29 @@ namespace ConsoleApp3
 {
     internal class Program
     {
-        enum DayWeek
+        /*enum DayWeek
         {
-            monday =1 ,
+            monday = 5 ,
             tuesday,
             wednsday,
             thursday,
-            friday,
+            friday = monday,
             saturday,
             sunday
         }
-
+        */
         static void Main(string[] args)
         {
-            DayWeek dayWeek = DayWeek.sunday;
+            /*DayWeek dayWeek = DayWeek.saturday;
             Console.WriteLine(dayWeek);
             Console.WriteLine((int)dayWeek);
             Console.WriteLine((DayWeek)1);
-
+            */
+        Console.WriteLine("Введите слово");
+            string str;
+        str = Console.ReadLine();
+        Console.WriteLine(str);
+        Console.WriteLine(str);
         }
     }
 }
