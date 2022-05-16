841d92110abb31f9559513670f584474ae46670e
Ваня Кориков <profftols@yandex.ru>
2022-05-16 21:32:09 +0500

    просто тренеровка для закрепления знаний

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -7,54 +7,25 @@ using System.Diagnostics;
 
 namespace ConsoleApp1
 {
-    class Program
+    class Sin
     {
-        static void Main(string[] args)
+        private int a { get; set; } = 22;
+        public void Som()
         {
-            Character cher = new Character();
-
-            Console.WriteLine("Меню создание персонажа: ");
-
-            cher = GetNew();
-
-            Console.WriteLine("Пресонаж создан!");
-            
-            cher.Print();
+            Console.WriteLine(a + 22);
+            return;
         }
 
-        static Character GetNew()
+        
+    }
+    class Program
+    {
+        static void Main(string[] gs)
         {
-            var cher = new Character();
+            Sin mob = new Sin();
 
-            cher.level = 1;
-
-            Console.WriteLine("Назовите персонажа");
-            string name = Console.ReadLine();
-            cher.name = name;
-
-            Console.WriteLine("Выберете рассу");
-            int i = 0;
-            var valu = Enum.GetValues(typeof(Race));
-            foreach (var item in valu)
-            {
-                i++;
-                Console.WriteLine($"{item} = {i}");
-            }    
-            int aa = int.Parse(Console.ReadLine());
-            cher.race = (Race)aa;
-
-            Console.WriteLine("Выберете класс");
-            var volu = Enum.GetValues(typeof(Spec));
-            int b = 0;
-            foreach (var item in volu)
-            {
-                b++;
-                Console.WriteLine($"{item} = {b}");
-            }
-            int bb = int.Parse(Console.ReadLine());
-            cher.spec = (Spec)bb;
-
-            return cher;
+            mob.Som();
+            
         }
     }
 }
