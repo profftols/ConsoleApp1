6d98c3e888790fe9cbb4e75c3e300753087a1de7
[GeNrI16] <[Denisow16@yandex.ru]>
2022-05-22 19:24:18 +0500

    dfg

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -8,34 +8,12 @@ namespace ConsoleApp1
         {
             string s = "пустота";
 
-            int num = 993;
+            int num = 9;
 
-            Console.WriteLine(Uni(s));
 
             Console.WriteLine($"{num}, {s}");
-        }
 
-        static string Uni(string sx)
-        {
-            //string n = s32;
 
-            sx = "xe21";
-
-            return sx;
-            
-            int num = 11;
-
-            Console.WriteLine(Uni(sx)); 
-
-            Uni(a, i, k);
-        }
-        static void Main(string[] args)
-        {
-            //int i = 0;
-            //int k = 0;
-            int[] sim = { 1, 4, 6, 7, 8, 9 };
-            Console.Write("вывод массива: ");
-            Uni(sim);
         }
     }
 }
