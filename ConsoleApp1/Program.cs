9e37362fd004f0b312b4a6b544d7d3ce42a55c7e
[GeNrI16] <[Denisow16@yandex.ru]>
2022-05-22 19:37:42 +0500

    FG

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -12,7 +12,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("E");
-            
+            Console.WriteLine("E");
         }
 
     }
