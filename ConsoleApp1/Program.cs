00f5da554e77b9f9ea138de1c89c9b4b999bf4c7
[GeNrI16] <[Denisow16@yandex.ru]>
2022-05-22 12:34:53 +0500

    tert

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -9,58 +9,9 @@ namespace ConsoleApp1
 {
     class Program
     {
-
-        enum Season
-        {
-            Winter,
-            Vesna,
-            Summer,
-            Autumn
-        }
         static void Main(string[] args)
         {
-            Random rand = new Random();
-
-            int[] temp = new int[9];
-            for (int i = 0; i < temp.Length; i++)
-            {
-                temp[i] = rand.Next(-25, 40);
-            }
-
-            //int[] temp = { -5, 0, 2, 6 };
-            Forchseason(temp);
-            
-        }
-        static void Forchseason(int[] temp)
-        {
-            int colt = 0;
-            int polo = 0;
-
-            for (int i = 0; i < temp.Length; i++)
-            {
-                if (temp[i] < 0)
-                {
-                    colt++;
-                }
-                if (temp[i] > 0)
-                {
-                    polo++;
-                }                           
-            }
-
-            if (colt > polo)
-            {
-                Console.WriteLine((Season)0);
-            }
-            if (polo > colt)
-            {
-                Console.WriteLine((Season)2);
-            }
-            if (colt == polo)
-            {
-                Console.WriteLine((Season)1);
-                Console.WriteLine((Season)3);
-            }
+            Console.WriteLine();
             
         }
 
--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -2,31 +2,69 @@
 
 namespace ConsoleApp3
 {
-    internal class Program
+
+    class Program
     {
-        /*enum DayWeek
+        enum Season
         {
-            monday = 5 ,
-            tuesday,
-            wednsday,
-            thursday,
-            friday = monday,
-            saturday,
-            sunday
+            winter = -1,
+            spring = 0,
+            summer = 1
         }
-        */
+
+        static void GetSeason(int[] par)
+        {
+            int a = 0;
+            int b = 0;
+
+            for (int i = 0; i < par.Length; i++)
+            {
+                if (par[i] >= 0)
+                {
+                    a += par[i];
+                }
+                else
+                {
+                    b += par[i];
+                }
+
+                if (i >= par.Length - 1)
+                {
+                    int v = a + b;
+
+                    if (v > 0)
+                    {
+                        Console.WriteLine((Season)1);
+                        return;
+                    }
+                    else if (v < 0)
+                    {
+                        Console.WriteLine(Season.winter);
+                        return;
+                    }
+                    else
+                    {
+                        Console.WriteLine(Season.spring);
+                        return;
+                    }
+                }
+            }
+
+        }
+
         static void Main(string[] args)
         {
-            /*DayWeek dayWeek = DayWeek.saturday;
-            Console.WriteLine(dayWeek);
-            Console.WriteLine((int)dayWeek);
-            Console.WriteLine((DayWeek)1);
+            /*Random rand = new Random();
+
+            int[] seas = new int[5];
+
+            for (int i = 0; i < seas.Length; i++)
+            {
+                seas[i] = rand.Next(-4, 4);
+            }
             */
-        Console.WriteLine("Введите слово");
-            string str;
-        str = Console.ReadLine();
-        Console.WriteLine(str);
-        Console.WriteLine(str);
+            int[] seas = { 0, 1, 5, 6 };
+            GetSeason(seas);
         }
     }
 }
