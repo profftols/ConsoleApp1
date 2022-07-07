59646be072e97402b02d7c20e3150fd449d61a06
profftols <profftols@HOME-PC>
2022-07-07 19:48:09 +0500

    Введение в С# урок 5

---
+++ ConsoleApp1/Person.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Person
+    {
+        public string Name { get; set; }
+        public string Profi { get; set; }
+        public string Mail { get; set; }
+        public string Tel { get; set; }
+        public int Solary { get; set; }
+        public int Age { get; set; }
+
+        public Person(string name, string profi, string mail, string tel, int solary, int age)
+        {
+            Name = name;
+            Profi = profi;
+            Mail = mail;
+            Tel = tel;
+            Solary = solary;
+            Age = age;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine($"{Name} {Profi} {Mail} {Tel} {Solary} {Age}");
+        }
+    }
+}
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.IO;
+
 
 namespace ConsoleApp1
 {
@@ -8,30 +10,48 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Выбери от 1 до 2");
-
-            int a = int.Parse(Console.ReadLine());
-            string s = "пустот1а";
-
-            int num = 93;
-
-            try
-            {
-                Choise(a);
-            }
-            catch (Exception)
-            {
-                throw;
-            }
-            
-            Console.WriteLine($"{num}, {s}");
-
-            Console.ReadLine(); 
+            /*Task1();
+            Task2();
+            Task3();*/
+            Task4();
         }
 
-        static int Choise(int a)
-        {
-            a = 3;
+        static void Task1 () {
+            string ask = Console.ReadLine();
+            File.WriteAllText("startup.txt", ask);
+        }
+
+        static void Task2() {
+            string ask = DateTime.Now.ToString();
+            File.AppendAllText("startup.txt", ask);
+        }
+
+        static void Task3() {
+            byte ask = byte.Parse(Console.ReadLine());
+            byte[] bytes = new byte[1];
+            bytes[0] = ask;
+            File.WriteAllBytes("bytes.bin", bytes);
+            byte[] fromFile = File.ReadAllBytes("bytes.bin");
+            for (int i = 0; i < fromFile.Length; i++) {
+                Console.Write($" {fromFile[i]}");
+            }
+        }
+
+        static void Task4() {
+            Person[] persArray = new Person[5];
+            persArray[0] = new Person("Ivan", "Engineer", "ivivan@mailbox.com", "892312312", 30000, 40);
+            persArray[1] = new Person("Popre", "Sionir", "ivivan@mailbox.com", "892312313", 45000, 33);
+            persArray[2] = new Person("Zolda", "Valadator", "ivivan@mailbox.com", "892312314", 35000, 70);
+            persArray[3] = new Person("Jun", "Direcrit", "ivivan@mailbox.com", "892312315", 22000, 25);
+            persArray[4] = new Person("Horker", "Joys", "ivivan@mailbox.com", "892312316", 34000, 55);
+
+            for (int i = 0; i < persArray.Length; i++)
+            {
+                if(persArray[i].Age > 40)
+                {
+                    persArray[i].Print(); 
+                }
+            }
         }
     }
 }
