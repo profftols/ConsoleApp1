962601173468ec9933af75cf10627a4b84612ec5
profftols <profftols@HOME-PC>
2022-07-16 18:44:14 +0500

    ГБ Урок 6 дз 1 и 2

--- ConsoleApp1/Person.cs
+++ ConsoleApp1/Person.cs
@@ -6,26 +6,6 @@ namespace ConsoleApp1
 {
     class Person
     {
-        public string Name { get; set; }
-        public string Profi { get; set; }
-        public string Mail { get; set; }
-        public string Tel { get; set; }
-        public int Solary { get; set; }
-        public int Age { get; set; }
-
-        public Person(string name, string profi, string mail, string tel, int solary, int age)
-        {
-            Name = name;
-            Profi = profi;
-            Mail = mail;
-            Tel = tel;
-            Solary = solary;
-            Age = age;
-        }
-
-        public void Print()
-        {
-            Console.WriteLine($"{Name} {Profi} {Mail} {Tel} {Solary} {Age}");
-        }
+       
     }
 }
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -2,56 +2,79 @@
 using System.Collections.Generic;
 using System.Text;
 using System.IO;
+using System.Diagnostics;
 
 
 namespace ConsoleApp1
 {
+
     class Program
     {
+
+        [Serializable]
+        public class MyArraySizeExeption : Exception { }
+        public class MyArrayDateExeption : Exception { }
         static void Main(string[] args)
         {
-            /*Task1();
-            Task2();
-            Task3();*/
-            Task4();
+            TaskManager();
+
+            string[,] array = new string[4, 4]
+                               { 
+                                { "3", "5", "3", "1" }, 
+                                { "3", "6", "3", "1" }, 
+                                { "5", "z", "6", "1" }, 
+                                { "2", "2", "1", "2" } 
+                               };
+
+            ArraySum(array);
+            //ArrayDataException();
+            //Task4();
         }
-
-        static void Task1 () {
-            string ask = Console.ReadLine();
-            File.WriteAllText("startup.txt", ask);
-        }
-
-        static void Task2() {
-            string ask = DateTime.Now.ToString();
-            File.AppendAllText("startup.txt", ask);
-        }
-
-        static void Task3() {
-            byte ask = byte.Parse(Console.ReadLine());
-            byte[] bytes = new byte[1];
-            bytes[0] = ask;
-            File.WriteAllBytes("bytes.bin", bytes);
-            byte[] fromFile = File.ReadAllBytes("bytes.bin");
-            for (int i = 0; i < fromFile.Length; i++) {
-                Console.Write($" {fromFile[i]}");
-            }
-        }
-
-        static void Task4() {
-            Person[] persArray = new Person[5];
-            persArray[0] = new Person("Ivan", "Engineer", "ivivan@mailbox.com", "892312312", 30000, 40);
-            persArray[1] = new Person("Popre", "Sionir", "ivivan@mailbox.com", "892312313", 45000, 33);
-            persArray[2] = new Person("Zolda", "Valadator", "ivivan@mailbox.com", "892312314", 35000, 70);
-            persArray[3] = new Person("Jun", "Direcrit", "ivivan@mailbox.com", "892312315", 22000, 25);
-            persArray[4] = new Person("Horker", "Joys", "ivivan@mailbox.com", "892312316", 34000, 55);
-
-            for (int i = 0; i < persArray.Length; i++)
+        
+        static void TaskManager() {
+            Process[] proc = Process.GetProcesses();
+            for (int i = 0, j = 0; i < proc.Length; i++, j++)
             {
-                if(persArray[i].Age > 40)
+                Console.WriteLine($"{j}, {proc[i].Id} - {proc[i].ProcessName} ");
+            }
+            Console.WriteLine("Введите id процесса для закрытие задачи");
+            int id = int.Parse(Console.ReadLine());
+            proc[id].Kill();
+        }
+        static void ArraySum(string[,] array) {
+
+            int sum = 0;
+
+            if (array.Length != 17) 
+            {
+                Console.WriteLine("Массив прывышает заданнных размером 4х4");
+                throw new MyArraySizeExeption();
+            }
+
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int k = 0; k < array.GetLength(1); k++)
                 {
-                    persArray[i].Print(); 
+                    try
+                    {
+                        sum += Int32.Parse(array[i, k]);
+                    } 
+                    catch
+                    {
+                        Console.WriteLine($"Ошибка в секторе массива: {i} - {k}, {array[i, k]} \nСумма массива: {sum}");
+                        throw new MyArrayDateExeption();
+                    }
                 }
             }
+            Console.WriteLine($"Сумма массива: {sum}");
         }
+        /*
+
+        static void ArrayDataException(string[,] array) {
+            
+        }
+        static void Task4() {
+            
+        }*/
     }
 }
