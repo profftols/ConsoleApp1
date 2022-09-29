2069fbb96d0f56234dfe830592ffce788e3b523e
Ivan <profftols@yandex.ru>
2022-09-29 15:58:40 +0500

    начало ДЗ-7

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,71 +10,166 @@ namespace ConsoleApp1
 
     class Program
     {
+        static int SIZE_X = 3;
+        static int SIZE_Y = 3;
 
-        [Serializable]
-        public class MyArraySizeExeption : Exception { }
-        public class MyArrayDateExeption : Exception { }
-        static void Main(string[] args)
+        static char[,] field = new char[SIZE_Y, SIZE_X];
+
+        static char PLAYER_DOT = 'X';
+        static char AI_DOT = 'O';
+        static char EMPTY_DOT = '.';
+
+        static Random random = new Random();
+
+        private static void InitField()
         {
-            TaskManager();
-
-            string[,] array = new string[4, 4]
-                               { 
-                                { "3", "5", "3", "1" }, 
-                                { "3", "6", "3", "1" }, 
-                                { "5", "z", "6", "1" }, 
-                                { "2", "2", "1", "2" } 
-                               };
-
-            ArraySum(array);
-            //ArrayDataException();
-            //Task4();
-        }
-        
-        static void TaskManager() {
-            Process[] proc = Process.GetProcesses();
-            for (int i = 0, j = 0; i < proc.Length; i++, j++)
+            for (int i = 0; i < SIZE_Y; i++)
             {
-                Console.WriteLine($"{j}, {proc[i].Id} - {proc[i].ProcessName} ");
-            }
-            Console.WriteLine("Введите id процесса для закрытие задачи");
-            int id = int.Parse(Console.ReadLine());
-            proc[id].Kill();
-        }
-        static void ArraySum(string[,] array) {
-
-            int sum = 0;
-
-            if (array.Length != 17) 
-            {
-                Console.WriteLine("Массив прывышает заданнных размером 4х4");
-                throw new MyArraySizeExeption();
-            }
-
-            for (int i = 0; i < array.GetLength(0); i++)
-            {
-                for (int k = 0; k < array.GetLength(1); k++)
+                for (int j = 0; j < SIZE_X; j++)
                 {
-                    try
+                    field[i, j] = EMPTY_DOT;
+                }
+            }
+        }
+
+        private static void PrintField()
+        {
+            Console.Clear();
+            Console.WriteLine("-------");
+            for (int i = 0; i < SIZE_Y; i++)
+            {
+                Console.Write("|");
+                for (int j = 0; j < SIZE_X; j++)
+                {
+                    Console.Write(field[i, j] + "|");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("-------");
+        }
+
+        private static void SetSym(int y, int x, char sym)
+        {
+            field[y, x] = sym;
+        }
+
+        private static bool IsCellValid(int y, int x)
+        {
+            if (x < 0 || y < 0 || x > SIZE_X - 1 || y > SIZE_Y - 1)
+            {
+                return false;
+            }
+
+            return field[y, x] == EMPTY_DOT;
+        }
+
+        private static bool IsFieldFull()
+        {
+            for (int i = 0; i < SIZE_Y; i++)
+            {
+                for (int j = 0; j < SIZE_X; j++)
+                {
+                    if (field[i, j] == EMPTY_DOT)
                     {
-                        sum += Int32.Parse(array[i, k]);
-                    } 
-                    catch
-                    {
-                        Console.WriteLine($"Ошибка в секторе массива: {i} - {k}, {array[i, k]} \nСумма массива: {sum}");
-                        throw new MyArrayDateExeption();
+                        return false;
                     }
                 }
             }
-            Console.WriteLine($"Сумма массива: {sum}");
+            return true;
         }
-        /*
 
-        static void ArrayDataException(string[,] array) {
-            
+        private static void playerMove()
+        {
+            int x, y;
+            do
+            {
+                Console.WriteLine("Координат по строке ");
+                Console.WriteLine("Введите координаты вашего хода в диапозоне от 1 до " + SIZE_Y);
+                x = Int32.Parse(Console.ReadLine()) - 1;
+                Console.WriteLine("Координат по столбцу ");
+                Console.WriteLine("Введите координаты вашего хода в диапозоне от 1 до " + SIZE_X);
+                y = Int32.Parse(Console.ReadLine()) - 1;
+            } while (!IsCellValid(y, x));
+            SetSym(y, x, PLAYER_DOT);
         }
-        static void Task4() {
-            
-        }*/
+
+        private static bool CheckWin(char sym)
+        {
+            if (field[0, 0] == sym && field[0, 1] == sym && field[0, 2] == sym)
+            {
+                return true;
+            }
+            if (field[1, 0] == sym && field[1, 1] == sym && field[1, 2] == sym)
+            {
+                return true;
+            }
+            if (field[2, 0] == sym && field[2, 1] == sym && field[2, 2] == sym)
+            {
+                return true;
+            }
+
+            if (field[0, 0] == sym && field[1, 0] == sym && field[2, 0] == sym)
+            {
+                return true;
+            }
+            if (field[0, 1] == sym && field[1, 1] == sym && field[2, 1] == sym)
+            {
+                return true;
+            }
+            if (field[0, 2] == sym && field[1, 2] == sym && field[2, 2] == sym)
+            {
+                return true;
+            }
+
+            if (field[0, 0] == sym && field[1, 1] == sym && field[2, 2] == sym)
+            {
+                return true;
+            }
+            if (field[2, 0] == sym && field[1, 1] == sym && field[0, 2] == sym)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private static void AiMove()
+        {
+            int x, y;
+            do
+            {
+                x = random.Next(0, SIZE_X);
+                y = random.Next(0, SIZE_Y);
+            } while (!IsCellValid(y, x));
+            SetSym(y, x, AI_DOT);
+        }
+
+
+        //static void Main(string[] args)
+        //{
+        //    InitField();
+        //    PrintField();
+        //    do
+        //    {
+        //        playerMove();
+        //        Console.WriteLine("Ваш ход на поле");
+        //        PrintField();
+        //        if (CheckWin(PLAYER_DOT))
+        //        {
+        //            Console.WriteLine("Вы выиграли");
+        //            break;
+        //        }
+        //        else if (IsFieldFull()) break;
+        //        AiMove();
+        //        Console.WriteLine("Ход Компа на поле");
+        //        PrintField();
+        //        if (CheckWin(AI_DOT))
+        //        {
+        //            Console.WriteLine("Выиграли Комп");
+        //            break;
+        //        }
+        //        else if (IsFieldFull()) break;
+        //    } while (true);
+        //    Console.WriteLine("!Конец игры!");
+        //}
     }
 }
