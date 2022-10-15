2a101679732074c9f35b6dc67f7c26be92beee1e
Ivan <profftols@yandex.ru>
2022-10-15 23:18:51 +0500

    исправление багов спираль

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,52 +4,115 @@ using System.Text;
 using System.IO;
 using System.Diagnostics;
 using System.Globalization;
+using System.Net;
+using System.Reflection;
 
 namespace ConsoleApp1
 {
-
     class Program
     {
-        static int x_size, y_size, num = 1;
 
-        static void Filling(int[,] mass) //spiral filling logic
+        static int x_size, y_size, num = 1, x, y;
+
+        static void Straight(ref int y, ref int x, int[,] mass)
         {
-            for (int y = 0; y < y_size; y++)
+            if (x != 0)
             {
-                if (y == 0)
+                do
                 {
-                    for (int x = 0; x < x_size; x++)
-                    {
-                        mass[x, y] = num++;
-                    }
-                }
+                    mass[y, x] = num++;
+                    x++;
+                } while (mass[y, x] == 0);
+                y++;
+                x--;
+            }
+            else
+            {
+                do
+                {
+                    mass[y, x] = ++num;
+                    x++;
+                } while (x < x_size);
+                y++;
             }
         }
-            
+
+        static void Side(ref int y, ref int x, int[,] mass)
+        {
+            if (x == x_size)
+            {
+                x--;
+                do
+                {
+                    mass[y, x] = num++;
+                    y++;
+                } while (y < y_size);
+                y--;
+                x--;
+            }
+            else
+            {
+                do
+                {
+                    mass[y, x] = num++;
+                    y++;
+                } while (mass[y, x] == 0);
+                y--;
+                x--;
+            }
+        }
+
+        static void Bottom(ref int y, ref int x, int[,] mass)
+        {
+            do
+            {
+                mass[y, x] = num++;
+                x--;
+            } while (x >= 0);
+            y--;
+            x++;
+        }
+
+        static void Middle(ref int y, ref int x, int[,] mass)
+        {
+            do
+            {
+                mass[y, x] = num++;
+                y--;
+            } while (mass[y, x] == 0);
+            y++;
+            x++;
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Задаим размер: \n1) X координат \n2) Y координат \n");
-            
-            x_size = int.Parse(Console.ReadLine());
-            y_size = int.Parse(Console.ReadLine());
-            
+            Console.WriteLine("Задаим размер: \n1) Y координат \n2) X координат \n");
+
+            x_size = 3; //int.Parse(Console.ReadLine());
+            y_size = 3; //int.Parse(Console.ReadLine());
+
             int[,] mass = new int[x_size, y_size];
-            /* Написать методы заполняющие: 
+
+            /* 
+             * Написать методы заполняющие: 
              * 1. по прямой плюс
              * 2. вниз плюс
              * 3. по прямой минус
              * 4. вверх минус
              * (реализовать проверку заполненности массива, иначе повторить)
              */
-            Filling(mass);
-            
-            Console.WriteLine();
 
-            for (int i = 0; i < y_size; i++) //array full check
+            Straight(ref y, ref x, mass);
+            Side(ref y, ref x, mass);
+            Bottom(ref y, ref x, mass);
+            Middle(ref y, ref x, mass);
+
+            Console.WriteLine();
+            for (int h = 0; h < x_size; h++)
             {
-                for (int j = 0; j < x_size; j++)
+                for (int j = 0; j < y_size; j++)
                 {
-                    Console.Write(mass[j,i]);
+                    Console.Write(mass[h, j]);
                 }
                 Console.WriteLine();
             }
