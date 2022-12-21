f6941b1a943540e33805ebf255f7164bfae36a3a
Ваня Кориков <profftols@yandex.ru>
2022-12-21 16:24:53 +0500

    алгоритмы 1

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,141 +1,75 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.IO;
-using System.Diagnostics;
-using System.Globalization;
-using System.Net;
-using System.Reflection;
 
 namespace ConsoleApp1
 {
     class Program
     {
-
-        static int x_size, y_size, num = 1, x, y;
-
-        static void Straight(ref int y, ref int x, int[,] mass)
-        {
-            if (x != 0)
-            {
-                do
-                {
-                    mass[y, x] = num++;
-                    x++;
-                } while (mass[y, x] == 0);
-                y++;
-                x--;
-            }
-            else
-            {
-                do
-                {
-                    mass[y, x] = num++;
-                    x++;
-                } while (x < y_size);
-                y++;
-            }
-        }
-
-        static void Side(ref int y, ref int x, int[,] mass)
-        {
-            if (x == y_size)
-            {
-                x--;
-                do
-                {
-                    mass[y, x] = num++;
-                    y++;
-                } while (y < x_size);
-                y--;
-                x--;
-            }
-            else if (mass[y,x] == 0)
-            {
-                do
-                {
-                    mass[y, x] = num++;
-                    y++;
-                } while (mass[y, x] == 0);
-                y--;
-                x--;
-            }
-        }
-
-        static void Bottom(ref int y, ref int x, int[,] mass)
-        {
-            if (y + 1 == x_size)
-            {
-                do
-                {
-                    mass[y, x] = num++;
-                    x--;
-                } while (x >= 0);
-                y--;
-                x++;
-            }
-            else
-            {
-                do
-                {
-                    mass[y, x] = num++;
-                    x--;
-                } while (mass[y, x] == 0);
-                y--;
-                x++;
-            }
-
-        }
-
-        static void Middle(ref int y, ref int x, int[,] mass)
-        {
-            do
-            {
-                mass[y, x] = num++;
-                y--;
-            } while (mass[y, x] == 0);
-            y++;
-            x++;
-        }
-
         static void Main(string[] args)
         {
-            Console.WriteLine("Задаим размер: \n1) Y координат \n2) X координат \n");
+            long n = long.Parse(Console.ReadLine());
 
-            x_size = 6; //int.Parse(Console.ReadLine());
-            y_size = 6; //int.Parse(Console.ReadLine());
+            long b = n;
 
-            int[,] mass = new int[x_size, y_size];
+            //NumberType(n); //1
 
-            /* 
-             * Написать методы заполняющие: 
-             * 1. по прямой плюс
-             * 2. вниз плюс
-             * 3. по прямой минус
-             * 4. вверх минус
-             * (реализовать проверку заполненности массива, иначе повторить)
-             */
+            //Console.WriteLine("\nсложность функции: 1+N*N*N+1+1+1N "); //2
+
+            Console.WriteLine("\nРекурсия:"); //3
+            FibonRecursion(n);
+            Console.WriteLine(n);
+
+            Console.WriteLine("\n\nЦикл:");
+            FibonCycle(b);
+        }
+
+        static void NumberType(long n)
+        {
+            Console.WriteLine("Начало\n");
+
+            long d = 0;
+            long i = 2;
+
             do
             {
-                Straight(ref y, ref x, mass);
-                Side(ref y, ref x, mass);
+                if (n % i == 0)
+                    d++;
 
-                if (mass[y, x] == 0)
-                    Bottom(ref y, ref x, mass);
-                if (mass[y, x] == 0)
-                    Middle(ref y, ref x, mass);
-            } while (mass[y, x] == 0);
+                i++;
 
-            
-            Console.WriteLine();
-            for (int h = 0; h < x_size; h++)
+            } while (i < n);
+
+            if (d == 0)
+                Console.WriteLine("Простое");
+            else
+                Console.WriteLine("Непростое");
+
+            Console.WriteLine("\nКонец");
+
+        }
+
+        static long FibonRecursion(long n)
+        {
+
+            if (n <2) return n;
+
+            return FibonRecursion(n - 1) + FibonRecursion(n - 2);
+        }
+
+
+        static void FibonCycle(long b)
+        {
+            int result = 0;
+            int j = 1;
+            int tmp;
+
+            for (int i = 0; i < b; i++)
             {
-                for (int j = 0; j < y_size; j++)
-                {
-                    Console.Write(mass[h, j]);
-                }
-                Console.WriteLine();
+                tmp = result;
+                result = j;
+                j += tmp;
             }
+
+            Console.WriteLine(result);
         }
     }
 }
